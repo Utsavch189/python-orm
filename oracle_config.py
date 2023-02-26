1457c749ac58739051dd7cb9c8d7d2fbeff697be
Utsav <utsavpokemon9000chatterjee@gmail.com>
2023-02-26 19:09:02 +0530

    first push

--- oracle_config.py
+++ oracle_config.py
@@ -7,8 +7,3 @@ HOST='localhost'
 PORT=1521
 SERVICE='XE'
 ENGINE_PATH_WIN_AUTH = DIALECT + '+' + SQL_DRIVER + '://' + USERNAME + ':' + PASSWORD +'@' + HOST + ':' + str(PORT) + '/?service_name=' + SERVICE
-def b():
-    return 5
-
-if __name__=='__main__':
-    b()
